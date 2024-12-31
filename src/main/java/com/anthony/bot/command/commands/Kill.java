71e099b0e14d0a2878a957227f88be3b91fb9f09
zeekomkommer <anouaranthonytel@gmail.com>
2024-12-31 21:51:45 +0100

    Update Kill.java

--- src/main/java/com/anthony/bot/command/commands/Kill.java
+++ src/main/java/com/anthony/bot/command/commands/Kill.java
@@ -30,6 +30,15 @@ public class Kill implements ICommand {
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
+        event.reply("Goodbye, I'll be back in a minute.").queue();
         System.exit(0);
+    }
+
+    private void delay() {
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException _) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
